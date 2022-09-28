7fa8a4f0b9a39a5cebb55115471ec37b4d622d22
Akanksha <23utopian.97@gmail.com>
2022-09-29 00:24:26 +0530

    day2 changes

---
+++ himalaya-app/src/Components/FirstBox.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import "../Styles/FirstBox.css"
+import {Box} from "@chakra-ui/react"
+
+const FirstBox = () => {
+  return (
+    <Box height="100px" width="100%" className="firstBox">
+        
+    </Box>
+  )
+}
+
+export default FirstBox
---
+++ himalaya-app/src/Routes/Home.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Navbar from '../Components/Navbar';
+import FirstBox from '../Components/FirstBox';
+
+const Home = () => {
+  return (
+    <div>
+        <Navbar/>
+        <FirstBox/>
+    </div>
+  )
+}
+
+export default Home
